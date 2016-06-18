5d6cf209cf6e677db8deff4d5740ea6a9de94f21
rples <rafal@ples.guru>
2016-06-18 09:49:45 +0200

    Created start project

---
+++ MobileRcp/MobileRcp.CoreTests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileRcp.CoreTests
+{
+    public class Class1
+    {
+    }
+}
