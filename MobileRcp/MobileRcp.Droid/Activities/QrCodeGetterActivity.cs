3e284f96e96a7d1e1a6c6ceb8b08e3ed1282f600
rples <rafal@ples.guru>
2016-06-26 21:17:52 +0200

    Created qr code getter activity

--- MobileRcp/MobileRcp.Droid/Activities/QrCodeGetterActivity.cs
+++ MobileRcp/MobileRcp.Droid/Activities/QrCodeGetterActivity.cs
@@ -9,17 +9,36 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using MobileRcp.Core.ViewModels;
+using ZXing.Mobile;
 
 namespace MobileRcp.Droid.Activities
 {
     [Activity(Label = "QrCodeGetterActivity")]
     public class QrCodeGetterActivity : Activity
     {
+        private QrCodeGetterViewModel _viewModel;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Create your application here
+            _viewModel = App.GetViewModelsFactory(Intent).GetQrCodeGetterViewModel();
+
+            MobileBarcodeScanner.Initialize(Application);
+
+            GetCode();
+        }
+
+        private async void GetCode()
+        {
+            var scanner = new MobileBarcodeScanner();
+            var result = await scanner.Scan();
+
+            if (result != null)
+            {
+                _viewModel.GetQrCodeCommand.Execute(result.Text);                
+            }
         }
     }
 }
